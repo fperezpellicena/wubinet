6a5735c9fea227d436dbc716d9ff692efabfa502
fperezpellicena <fperezpellicena@gmail.com>
2014-10-26 18:48:13 +0100

    Configurable properties

---
+++ src/main/java/com/wubinet/ConfigProperties.java
@@ -0,0 +1,33 @@
+package com.wubinet;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties {
+
+	private static final String PROPERTIES_FILE = "config.properties";
+
+	private static final Properties PROPERTIES = new Properties();
+
+	static {
+		try {
+			PROPERTIES.load(new ClassPathResource(PROPERTIES_FILE).getInputStream());
+		} catch (IOException ex) {
+			throw new IllegalStateException("Configuration properties cannot be readed", ex);
+		}
+	}
+
+	private ConfigProperties() {
+	}
+
+	public static String xbeePort() {
+		return PROPERTIES.getProperty("xbee.port");
+	}
+
+	public static int xbeeBaudrate() {
+		return Integer.parseInt(PROPERTIES.getProperty("xbee.baudrate"));
+	}
+
+}
--- src/main/java/com/wubinet/Main.java
+++ src/main/java/com/wubinet/Main.java
@@ -3,11 +3,11 @@ package com.wubinet;
 import com.rapplogic.xbee.api.XBee;
 import com.rapplogic.xbee.api.XBeeException;
 import com.wubinet.xbee.XbeePacketListener;
-import gnu.io.CommPortIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Enumeration;
+import static com.wubinet.ConfigProperties.xbeeBaudrate;
+import static com.wubinet.ConfigProperties.xbeePort;
 
 public class Main {
 
@@ -16,7 +16,7 @@ public class Main {
 	public static void main(String[] args) throws XBeeException {
 		LOG.info("Booting Wubinet service");
 		XBee xbee = new XBee();
-		xbee.open("/dev/ttyUSB0", 9600);
+		xbee.open(xbeePort(), xbeeBaudrate());
 		xbee.addPacketListener(new XbeePacketListener());
 	}
 }
